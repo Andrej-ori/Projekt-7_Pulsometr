900f30d4471d9e57aa9db22d818d76b4842474cf
Andrej-ori <a31338805@gmail.com>
2024-11-10 12:17:49 +0300

    creating a heart rate monitor project

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+//ПОДКЛЮЧЕНИЕ ЗАВИСИМОСТЕЙ
+
+// подключаем gulp
+const gulp				= require('gulp');
+// подключаем browserSync
+const browserSync = require('browser-sync');
+// подключаем gulp-sass
+const sass = require('gulp-sass')(require('sass'));
+// подключаем cleanCSS
+const cleanCSS = require('gulp-clean-css');
+// подключаем autoprefixer
+const autoprefixer = require('gulp-autoprefixer');
+// подключаем gulp-rename
+const rename = require("gulp-rename");
+
+
+// Static server (настройка сервера)
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			baseDir: "src"
+		}
+	});
+});
+
+
+// компиляция sass|scss файлов в css файлы
+gulp.task('styles', function(){
+	return gulp.src("src/sass/**/*.+(scss|sass)")
+				.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+				.pipe(rename({
+					prefix: "",
+					suffix: ".min",
+				}))
+				.pipe(autoprefixer({
+					browsers: ['last 2 versions'],
+					cascade: false
+				}))
+				.pipe(cleanCSS({compatibility: 'ie8'}))
+				.pipe(gulp.dest("src/css"))
+				.pipe(browserSync.stream());
+});
+
+
+// отслеживание изменений в файлах sass|scss и html файлах
+gulp.task('watch', function(){
+	gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+	gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+
+// запуск всех задачь
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
