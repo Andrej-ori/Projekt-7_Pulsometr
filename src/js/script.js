7d653c4946d797874662548ede5271ae88b9e2fe
Andrej-ori <a31338805@gmail.com>
2024-11-26 00:14:40 +0300

    Добавление секции с Табами, ее стилизация и написание скриптов для переключения между табами и внутри карточки товара

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,71 @@
 $(document).ready(function(){
 	$('.carousel__inner').slick({
-		// установка скорости перемотки слайдера в миллисекундах
+		//? установка скорости перемотки слайдера в миллисекундах
 		speed: 1200,
-		// установка адаптации размера изображения слайда
+		//? установка адаптации размера изображения слайда
 		// adaptiveHeight: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/chevron_left_solid.png"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/chevron_right_solid.png"></button>',
-		// установка свойств при изменении размера экрана
+		//? установка свойств при изменении размера экрана
 		responsive: [
 			{
-				// контрольная точка расширения
+				//* контрольная точка расширения
 				breakpoint: 992,
-				// свойства
+				//* свойства
 				settings: {
-					// точки для скроллинга включены
+					//* точки для скроллинга включены
 					dots: true,
-					// стрелки для перемотки выключены
+					//* стрелки для перемотки выключены
 					arrows: false
 				}
 			}
 		]
 	});
+
+//! СКИПТ ДЛЯ ТЕРЕКЛЮЧЕНИЯ ТАБОВ 
+
+	$("ul.catalog__tabs").on("click", "li:not(.catalog__tab--active)", function () {
+		$(this)
+			.addClass("catalog__tab--active")
+			.siblings()
+			.removeClass("catalog__tab--active")
+			.closest("div.container")
+			.find("div.catalog__content")
+			.removeClass("catalog__content--active")
+			.eq($(this).index())
+			.addClass("catalog__content--active");
+	});
+
+//! НЕ ОПТИМИЗИРОВАННО
+
+	// $(".catalog-item__link").each(function (i) {
+	// 	$(this).on("click", function (e) {
+	// 		e.preventDefault();
+	// 		$(".catalog-item__content").eq(i).toggleClass("catalog-item__content--active");
+	// 		$(".catalog-item__list").eq(i).toggleClass("catalog-item__list--active");
+	// 	});
+	// });
+
+	// $(".catalog-item__back").each(function (i) {
+	// 	$(this).on("click", function (e) {
+	// 		e.preventDefault();
+	// 		$(".catalog-item__content").eq(i).toggleClass("catalog-item__content--active");
+	// 		$(".catalog-item__list").eq(i).toggleClass("catalog-item__list--active");
+	// 	});
+	// });
+
+//! ОПТИМИЗИРОВАННО
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on("click", function (e) {
+				e.preventDefault();
+				$(".catalog-item__content").eq(i).toggleClass("catalog-item__content--active");
+				$(".catalog-item__list").eq(i).toggleClass("catalog-item__list--active");
+			});
+		});
+	}
+
+	toggleSlide(".catalog-item__back");
+	toggleSlide(".catalog-item__link");
 });
