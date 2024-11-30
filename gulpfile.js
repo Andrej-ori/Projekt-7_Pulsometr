5b3b3f457720e901d94e9b7f2c4d3dd81b973a6f
Andrej-ori <a31338805@gmail.com>
2024-11-30 19:53:54 +0300

    изменеие gupfile.js для отслеживания изменений в js файлах

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,61 @@
 //ПОДКЛЮЧЕНИЕ ЗАВИСИМОСТЕЙ
 
 // подключаем gulp
-const gulp				= require('gulp');
+const gulp					= require('gulp');
 // подключаем browserSync
-const browserSync = require('browser-sync');
+const browserSync 	= require('browser-sync');
 // подключаем gulp-sass
-const sass = require('gulp-sass')(require('sass'));
+const sass 					= require('gulp-sass')(require('sass'));
 // подключаем cleanCSS
-const cleanCSS = require('gulp-clean-css');
+const cleanCSS 			= require('gulp-clean-css');
 // подключаем autoprefixer
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer  = require('gulp-autoprefixer');
 // подключаем gulp-rename
-const rename = require("gulp-rename");
+const rename 				= require("gulp-rename");
 
 
 // Static server (настройка сервера)
 gulp.task('server', function() {
 	browserSync.init({
 		server: {
-			baseDir: "src"
+			baseDir: 'src'
 		}
 	});
+	gulp.watch('src/*.html').on('change', browserSync.reload);
 });
-
 
 // компиляция sass|scss файлов в css файлы
 gulp.task('styles', function(){
-	return gulp.src("src/sass/**/*.+(scss|sass)")
+	return gulp.src('src/sass/**/*.+(scss|sass|css)')
 				.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-				.pipe(rename({
-					prefix: "",
-					suffix: ".min",
-				}))
+				.pipe(rename({ prefix: '', suffix: '.min' }))
 				.pipe(autoprefixer())
 				.pipe(cleanCSS({compatibility: 'ie8'}))
-				.pipe(gulp.dest("src/css"))
+				.pipe(gulp.dest('src/css'))
 				.pipe(browserSync.stream());
 });
 
+// Отслеживание изменений в файлах js
+gulp.task('scripts', function () {
+	return gulp.src('src/js/**/*.js')
+				.pipe(gulp.dest('dist/js'))
+				.pipe(browserSync.stream());
+});
 
 // отслеживание изменений в файлах sass|scss и html файлах
-gulp.task('watch', function(){
-	gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-	gulp.watch("src/*.html").on("change", browserSync.reload);
+
+// gulp.task('watch', function(){
+// 	gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+// 	gulp.watch("src/*.html").on("change", browserSync.reload);
+// });
+
+
+gulp.task('watch', function () {
+	gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+	gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'));
+	// gulp.watch("src/*.html").on("change", gulp.parallel("html"));
 });
 
 
 // запуск всех задачь
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles',));
